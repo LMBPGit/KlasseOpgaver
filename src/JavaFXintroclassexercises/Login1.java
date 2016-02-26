e72bff3c86fecb9276e767c77fd74a3366b41079
MaskedPotato <lucasmbpoulsen@hotmail.com>
2016-02-26 08:58:03 +0100

    Login1 started with Jeff

---
+++ src/JavaFXintroclassexercises/Login1.java
@@ -0,0 +1,16 @@
+package JavaFXintroclassexercises;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Login1 extends Application{
+
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
